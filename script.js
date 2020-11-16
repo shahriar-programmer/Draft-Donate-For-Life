6bf371e2ed385c3dcb4cd73aa951c86920599323
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2020-11-16 19:41:33 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// Open the full screen search box
+function openSearch() {
+  document.getElementById("mob_search__overlay").style.display = "block";
+}
+
+// Close the full screen search box
+function closeSearch() {
+  document.getElementById("mob_search__overlay").style.display = "none";
+}
